7d50b07161d3f3f34ed05cf378d41a30a35b1e39
Andriy <andre.ixar@gmail.com>
2021-08-20 09:50:56 -0400

    2 more bindings

---
+++ src/nyx/__init__.py
@@ -0,0 +1,3 @@
+from interface import is_nyx_alive
+from interface import is_nyx_alive2
+from interface import pixel_intensity_stats
--- src/nyx/interface.py
+++ src/nyx/interface.py
@@ -1,8 +1,5 @@
 import sys
-#XXXXXXXX	import backend
-
-import os
-TestData = os.path.join(os.path.dirname(os.path.realpath(__file__)), "testdata.bin")
+import numpy as np
 
 def is_nyx_alive():
 	print ("is_nyx_alive responded")
@@ -12,3 +9,85 @@ def is_nyx_alive2(arg):
 	print ("is_nyx_alive2 received " + str(arg))
 	return True	
 	
+def pixel_intensity_stats(label_dir, intensity_dir):
+    """Calculate mean, median, min, max, range, standard_deviation, skewness, kurtosis, 
+    mean_absolute_deviation, energy, root_mean_squared, entropy, mode, uniformity, 
+    P10, P25, P75, P90, interquartile_range, robust_mean_absolute_deviation, 
+    weighted_centroid_y, weighted_centroid_x 
+
+    Parameters
+    ----------
+    label_dir : path to the label images directory, type string
+    intensity_dir : path to the intensity images directory, type string
+
+    Returns
+    -------
+    numpy.ndarray, type float - 2-dimensional array of N calculated features 
+        for each of L unique labels in shape 
+        `[[f1, f2, ... fN], [f1, f2, ... fN], ... [f1, f2, ... fN]]`.
+    """
+
+    """
+    assert (bool(label_dir and not label_dir.isspace()))
+    assert (bool(intensity_dir and not intensity_dir.isspace()))
+
+    f = calc_pixel_intensity_stats (label_dir, intensity_dir)
+    """
+
+    f = np.array( [[ 1, 2, 3],
+                 [ 4, 2, 5]] )
+
+    return f
+
+def bounding_box(label_dir, intensity_dir):
+    """Calculate ROIs' bounding box x, y, width, and height  
+
+    Parameters
+    ----------
+    label_dir : path to the label images directory, type string
+    intensity_dir : path to the intensity images directory, type string
+
+    Returns
+    -------
+    numpy.ndarray, type float - 2-dimensional array of N calculated features 
+        for each of L unique labels in shape 
+        `[[f1, f2, ... fN], [f1, f2, ... fN], ... [f1, f2, ... fN]]`.
+    """
+
+    assert (bool(label_dir and not label_dir.isspace()))
+    assert (bool(intensity_dir and not intensity_dir.isspace()))
+
+    f = calc_bounding_box (label_dir, intensity_dir)
+    return f
+
+
+def feret(label_dir, intensity_dir):
+    """Calculate ROIs' Feret diameters, angles, and statistics  
+
+    Parameters
+    ----------
+    label_dir : path to the label images directory, type string
+    intensity_dir : path to the intensity images directory, type string
+
+    Returns
+    -------
+    numpy.ndarray, type float - 2-dimensional array of N calculated features 
+    	    MIN_FERET_DIAMETER
+		    MAX_FERET_DIAMETER
+		    MIN_FERET_ANGLE
+		    MAX_FERET_ANGLE
+		    STAT_FERET_DIAM_MIN
+		    STAT_FERET_DIAM_MAX
+		    STAT_FERET_DIAM_MEAN
+		    STAT_FERET_DIAM_MEDIAN
+		    STAT_FERET_DIAM_STDDEV
+		    STAT_FERET_DIAM_MODE
+        for each of L unique labels in shape 
+        `[[f1, f2, ... fN], [f1, f2, ... fN], ... [f1, f2, ... fN]]`.
+    """
+
+    assert (bool(label_dir and not label_dir.isspace()))
+    assert (bool(intensity_dir and not intensity_dir.isspace()))
+
+    f = calc_bounding_feret (label_dir, intensity_dir)
+    return f
