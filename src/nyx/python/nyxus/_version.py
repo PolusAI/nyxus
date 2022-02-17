34aa418001c5ed9dffab9fc8ba2ba2991c1aa38d
Sameeul B Samee <ssamee@simscale.com>
2022-02-17 13:00:06 -0500

    Docker

--- src/nyx/python/nyxus/_version.py
+++ src/nyx/python/nyxus/_version.py
@@ -41,7 +41,7 @@ def get_config():
     # _version.py
     cfg = VersioneerConfig()
     cfg.VCS = "git"
-    cfg.style = "pep440"
+    cfg.style = "pep440-pre"
     cfg.tag_prefix = ""
     cfg.parentdir_prefix = "None"
     cfg.versionfile_source = "src/nyx/python/nyxus/_version.py"
