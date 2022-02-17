8e4cbcbb0d72372ba00a957aab320d2502f429d9
Sameeul B Samee <ssamee@simscale.com>
2022-02-17 14:06:34 -0500

    fix versioneer

--- src/nyx/python/nyxus/_version.py
+++ src/nyx/python/nyxus/_version.py
@@ -41,7 +41,7 @@ def get_config():
     # _version.py
     cfg = VersioneerConfig()
     cfg.VCS = "git"
-    cfg.style = "pep440-pre"
+    cfg.style = "pep440"
     cfg.tag_prefix = ""
     cfg.parentdir_prefix = "None"
     cfg.versionfile_source = "src/nyx/python/nyxus/_version.py"
