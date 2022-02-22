0eb090cf548a4d7c9734484e39aa23aeb155f85f
Andriy <andre.ixar@gmail.com>
2022-02-21 20:42:23 -0500

    Optimized log in GLCM

--- src/nyx/helpers/helpers.h
+++ src/nyx/helpers/helpers.h
@@ -123,5 +123,54 @@ namespace Nyxus
 	//	}
 	//
 
+	inline double fast_log10 (double _x)  // compute log2(x) by reducing x to [0.75, 1.5)
+	{
+		float x = _x;
+
+		// a*(x-1)^2 + b*(x-1) approximates log2(x) when 0.75 <= x < 1.5
+		const float a = -.6296735;
+		const float b = 1.466967;
+		float signif, fexp;
+		int exp;
+		float lg2;
+		union { float f; unsigned int i; } ux1, ux2;
+		int greater;		// actually, a boolean 
+
+		//
+		// Assuming IEEE representation, which is sgn(1):exp(8):frac(23)
+		// representing (1+frac)*2^(exp-127)  Call 1+frac the significand
+		//
+
+		 // get exponent
+		ux1.f = x;
+		exp = (ux1.i & 0x7F800000) >> 23;	// actual exponent is exp-127, will subtract 127 later
+
+		greater = ux1.i & 0x00400000;  // true if signif > 1.5
+		if (greater)
+		{
+			// signif >= 1.5 so need to divide by 2.  Accomplish this by 
+			// stuffing exp = 126 which corresponds to an exponent of -1 
+			ux2.i = (ux1.i & 0x007FFFFF) | 0x3f000000;
+			signif = ux2.f;
+			fexp = exp - 126.0f;    // 126 instead of 127 compensates for division by 2
+			signif = signif - 1.0f;                    // <
+			lg2 = fexp + a * signif * signif + b * signif;  // <
+		}
+		else
+		{
+			// get signif by stuffing exp = 127 which corresponds to an exponent of 0
+			ux2.i = (ux1.i & 0x007FFFFF) | 0x3f800000;
+			signif = ux2.f;
+			fexp = exp - 127.0f;
+			signif = signif - 1.0f;                    // <<--
+			lg2 = fexp + a * signif * signif + b * signif;  // <<--
+		}
+
+		// lines marked <<-- are common code, but optimize better 
+		//  when duplicated, at least when using gcc
+
+		return lg2 * 0.30102999566;	// log2 to log10
+	}
+
 }
 
