6fab3765d85221882246760034aa0b5aa8c993e8
sameeul <sameeul@gmail.com>
2022-06-28 16:54:13 -0400

    windows build

--- install_win_nyxus.bat
+++ install_win_nyxus.bat
@@ -3,7 +3,7 @@ set PATH=%LIB_PATH%;%PATH%
 echo %LIB_PATH%
 echo %PATH%
 python -m pip install delvewheel
-python setup.py bdist_wheel -d dist -b tmp
+python setup.py bdist_wheel -d dist
 pushd dist
 for %%j in (*.whl) do (
   delvewheel repair %%j
