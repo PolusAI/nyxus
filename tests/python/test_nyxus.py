cebba35767fc815b1ee728d96daf04e3a4d367fc
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2022-07-01 09:39:00 -0400

    Update gpu tests

--- tests/python/test_nyxus.py
+++ tests/python/test_nyxus.py
@@ -17,28 +17,34 @@ class TestImport():
         assert nyxus.__name__ == "nyxus" 
         
 class TestNyxus():
-    
-    PATH = PATH = Path(__file__).with_name('data')
+        PATH = PATH = Path(__file__).with_name('data')
 
-    def test_gabor_gpu(self):
-        urls = ['https://github.com/stardist/stardist/releases/download/0.1.0/dsb2018.zip']
-        dir_names = ['dsb2018']
+        def test_gabor_gpu(self):
+                urls = ['https://github.com/stardist/stardist/releases/download/0.1.0/dsb2018.zip']
+                dir_names = ['dsb2018']
+                
+                download_datasets(urls, dir_names)
+                
+                for dset in dir_names:
+                    
+                    intens = str(self.PATH/dset/"train/images")
+                    seg = str(self.PATH/dset/"train/masks")
+                    
+                    # cpu gabor
+                    cpu_nyx = nyxus.Nyxus(["GABOR"])
+                    if (nyxus.gpu_is_available()):
+                        cpu_nyx.using_gpu(False)
+                    cpu_features = cpu_nyx.featurize(intens, seg)
+
+                    if (nyxus.gpu_is_available()):
+                        # gpu gabor
+                        gpu_nyx = nyxus.Nyxus(["GABOR"])
+                        gpu_nyx.using_gpu(True)
+                        gpu_features = gpu_nyx.featurize(intens, seg)
+                        
+                        assert gpu_features.equals(cpu_features)
+                    else:
+                        print("Gpu not available")
+                        assert True
+                
         
-        download_datasets(urls, dir_names)
-        
-        for dset in dir_names:
-            
-            intens = str(self.PATH/dset/"train/images")
-            seg = str(self.PATH/dset/"train/masks")
-            
-            # cpu gabor
-            cpu_nyx = nyxus.Nyxus(["GABOR"])
-            cpu_nyx.using_gpu(False)
-            cpu_features = cpu_nyx.featurize(intens, seg)
-  
-            # gpu gabor
-            gpu_nyx = nyxus.Nyxus(["GABOR"])
-            gpu_nyx.using_gpu(True)
-            gpu_features = gpu_nyx.featurize(intens, seg)
-            
-            assert gpu_features.equals(cpu_features)
--- tests/testAll.cc
+++ tests/testAll.cc
@@ -1,12 +1,17 @@
 #include <gtest/gtest.h>
 #include "testDummy.h"
+#include "test_gabor.h"
+#include "../src/nyx/environment.h"
+#include "../src/nyx/globals.h"
 
 TEST(TEST_NYXUS, DUMMY_TEST){
   test_dummy_function();
 }
 
 TEST(TEST_GABOR_GPU, DSB2018){
-  test_gabor_gpu_2018();
+  #ifdef USE_GPU
+    if(theEnvironment.using_gpu()) test_gabor_gpu_2018();
+  #endif
 }
 
 int main(int argc, char **argv) {
