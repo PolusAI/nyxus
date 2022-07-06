d3c43ae6271808a47bdbd56c9311df1a7eb9c2ab
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2022-07-06 12:40:02 -0400

    Update test data download

--- tests/testAll.cc
+++ tests/testAll.cc
@@ -1,14 +1,16 @@
 #include <gtest/gtest.h>
 #include "testDummy.h"
 #include "test_gabor.h"
+#include "test_download_data.h"
 #include "../src/nyx/environment.h"
 #include "../src/nyx/globals.h"
 
 TEST(TEST_NYXUS, DUMMY_TEST){
-  test_dummy_function();
+  test_dummy_functiotestn();
 }
 
 TEST(TEST_GABOR_GPU, DSB2018){
+  get("https://github.com/stardist/stardist/releases/download/0.1.0/dsb2018.zip", "dsb2018");
   #ifdef USE_GPU
     test_gabor_gpu_2018();
   #endif
--- tests/test_download_data.cc
+++ tests/test_download_data.cc
@@ -1,17 +1,13 @@
 #include "test_download_data.h"
-#include <curl/curl.h>
 
 using namespace std;
 
 int get(const string& url, const string& filename){
-    string cmd = "wget " + url;
+
+    string cmd = "cd .. && python3 tests/download_data.py --url \"" + url + "\" --filename \"" + filename + "\"";
     
     const char* cmd_ptr = cmd.c_str();
-    system(cmd_ptr);
-
-    cmd = "unzip " + filename;
-    cmd_ptr = cmd.c_str();
-
+   
     system(cmd_ptr);
 
     return 0;
