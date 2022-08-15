c73b9bb8c8cafa60cbff3832b65cf7108ed3eacd
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2022-08-15 11:44:07 -0600

    Update test_download_data.cc

--- tests/test_download_data.cc
+++ tests/test_download_data.cc
@@ -4,11 +4,15 @@ using namespace std;
 
 int get(const string& url, const string& filename){
 
-    string cmd = "cd .. && python3 tests/download_data.py --url \"" + url + "\" --filename \"" + filename + "\"";
+    string cmd = "pwd";
     
     const char* cmd_ptr = cmd.c_str();
-   
+
     system(cmd_ptr);
 
-    return 0;
-}
+    cmd = "cd ../tests/ && python3 download_data.py --url \"" + url + "\" --filename \"" + filename + "\"";
+    
+    cmd_ptr = cmd.c_str();
+   
+    return system(cmd_ptr);
+} 
