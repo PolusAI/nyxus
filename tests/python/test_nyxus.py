f17245f2f907b94435a63d1b04549aaa3fb39088
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2023-02-14 07:45:17 -0700

    Update test_nyxus.py

--- tests/python/test_nyxus.py
+++ tests/python/test_nyxus.py
@@ -1,50 +1,38 @@
 import nyxus
 import pytest
 import time
+import numpy as np
 from pathlib import Path
 from test_download_data import download
 
-def test_import():
-    assert nyxus.__name__ == "nyxus" 
-    
-def download_datasets(urls, dir_names):
-    for i in range(len(urls)):
-        download(urls[i], dir_names[i])
-        
+intens = np.array([[[1, 4, 4, 1, 1],
+        [1, 4, 6, 1, 1],
+        [4, 1, 6, 4, 1],
+        [4, 4, 6, 4, 1]],
+                   
+        [[0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]]       
+                   
+                   ])
 
-class TestImport():
-    def test_import(self):
-        assert nyxus.__name__ == "nyxus" 
-        
-class TestNyxus():
-        PATH = PATH = Path(__file__).with_name('data')
+seg = np.array([[1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1]])
 
-        def test_gabor_gpu(self):
-                urls = ['https://github.com/stardist/stardist/releases/download/0.1.0/dsb2018.zip']
-                dir_names = ['dsb2018']
-                
-                download_datasets(urls, dir_names)
-                
-                for dset in dir_names:
-                    
-                    intens = str(self.PATH/dset/"train/images")
-                    seg = str(self.PATH/dset/"train/masks")
-                    
-                    # cpu gabor
-                    cpu_nyx = nyxus.Nyxus(["GABOR"])
-                    if (nyxus.gpu_is_available()):
-                        cpu_nyx.using_gpu(False)
-                    cpu_features = cpu_nyx.featurize_directory(intens, seg)
-
-                    if (nyxus.gpu_is_available()):
-                        # gpu gabor
-                        gpu_nyx = nyxus.Nyxus(["GABOR"], using_gpu=0)
-                        gpu_nyx.using_gpu(True)
-                        gpu_features = gpu_nyx.featurize_directory(intens, seg)
-                        
-                        assert gpu_features.equals(cpu_features)
-                    else:
-                        print("Gpu not available")
-                        assert True
+# cpu gabor
+cpu_nyx = nyxus.Nyxus(["*ALL_INTENSITY*"])
+#if (nyxus.gpu_is_available()):
+#    cpu_nyx.using_gpu(False)
+print()
+print(intens)
+print()
+print(seg)
+print()
+cpu_features = cpu_nyx.featurize_memory(intens, seg)
+#print("finished features")
+print(cpu_features)
                 
         
