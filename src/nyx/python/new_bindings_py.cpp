469d5c4482d511391797147cf37d10164229e12c
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2023-08-16 14:15:01 -0600

    Split arrow backend into separate pybind module

--- src/nyx/python/new_bindings_py.cpp
+++ src/nyx/python/new_bindings_py.cpp
@@ -642,15 +642,6 @@ bool arrow_is_enabled_imp() {
 PYBIND11_MODULE(backend, m)
 {
 
-#ifdef USE_ARROW
-    Py_Initialize();
-
-    int success = arrow::py::import_pyarrow();
-
-    if (success != 0) {
-        throw std::runtime_error("Error initializing pyarrow.");
-    } 
-#endif
     m.doc() = "Nyxus";
     
     m.def("initialize_environment", &initialize_environment, "Environment initialization");
@@ -668,12 +659,30 @@ PYBIND11_MODULE(backend, m)
     m.def("set_if_ibsi_imp", &set_if_ibsi_imp, "Set if the features will be ibsi compliant");
     m.def("set_environment_params_imp", &set_environment_params_imp, "Set the environment variables of Nyxus");
     m.def("get_params_imp", &get_params_imp, "Get parameters of Nyxus");
-    m.def("create_arrow_file_imp", &create_arrow_file_imp, "Creates an arrow file for the feature calculations");
-    m.def("get_arrow_file_imp", &get_arrow_file_imp, "Get path to arrow file");
-    m.def("get_parquet_file_imp", &get_parquet_file_imp, "Returns path to parquet file");
-    m.def("create_parquet_file_imp", &create_parquet_file_imp, "Create parquet file for the features calculations");
-    m.def("get_arrow_table_imp", &get_arrow_table_imp, py::call_guard<py::gil_scoped_release>());
-    m.def("arrow_is_enabled_imp", &arrow_is_enabled_imp, "Check if arrow is enabled.");
+}
+
+PYBIND11_MODULE(backend_arrow, m_arrow)
+{
+
+#ifdef USE_ARROW
+    Py_Initialize();
+
+    int success = arrow::py::import_pyarrow();
+
+    if (success != 0) {
+        throw std::runtime_error("Error initializing pyarrow.");
+    } 
+#endif
+
+    m_arrow.doc() = "Nyxus Arrow";
+
+    m_arrow.def("create_arrow_file_imp", &create_arrow_file_imp, "Creates an arrow file for the feature calculations");
+    m_arrow.def("get_arrow_file_imp", &get_arrow_file_imp, "Get path to arrow file");
+    m_arrow.def("get_parquet_file_imp", &get_parquet_file_imp, "Returns path to parquet file");
+    m_arrow.def("create_parquet_file_imp", &create_parquet_file_imp, "Create parquet file for the features calculations");
+    m_arrow.def("get_arrow_table_imp", &get_arrow_table_imp, py::call_guard<py::gil_scoped_release>());
+    m_arrow.def("arrow_is_enabled_imp", &arrow_is_enabled_imp, "Check if arrow is enabled.");
+
 }
 
 ///
