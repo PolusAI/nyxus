b4f5e8f9e0dee541aab2b0bd99272a2034c3c022
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2023-11-14 12:58:01 -0700

    Add filename option to Arrow and Parquet writers

--- src/nyx/arrow_output_stream.h
+++ src/nyx/arrow_output_stream.h
@@ -30,6 +30,7 @@ private:
 public:
     std::tuple<bool, std::optional<std::string>> create_arrow_file(const Nyxus::SaveOption& arrow_file_type,
                                                          const std::string& arrow_file_path,
+                                                         const std::string& output_filename,
                                                          const std::vector<std::string>& header);
     std::shared_ptr<arrow::Table> get_arrow_table(const std::string& file_path);
     std::string get_arrow_path();
@@ -50,6 +51,7 @@ class ArrowOutputStream {
 public:
     std::tuple<bool, std::optional<std::string>> create_arrow_file(const Nyxus::SaveOption& arrow_file_type,
                                                          const std::string& arrow_file_path,
+                                                         const std::string& output_filename,
                                                          const std::vector<std::string>& header);
     std::tuple<bool, std::optional<std::string>> write_arrow_file (const std::vector<std::tuple<std::vector<std::string>, int, std::vector<double>>>& features);
     std::tuple<bool, std::optional<std::string>> close_arrow_file ();
