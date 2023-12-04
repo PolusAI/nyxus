bfbcb3facd65978a13ef5e0f3b76f0f1675cbf16
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2023-12-04 09:36:55 -0700

    Fix docstrings

--- src/nyx/python/nyxus/nyxus.py
+++ src/nyx/python/nyxus/nyxus.py
@@ -239,10 +239,10 @@ class Nyxus:
             Output format for the features values. Valid options are "pandas", "arrowipc", and "parquet".
         output_path: str (optional, default "")
             Output filepath for Arrow IPC and Parquet output formats. Default is "", which is the current directory.
-            The output_path can either be to a directory or filename. For example:
-            -If 'output_path=/path/to/directory' then a file with the default name NyxusFeatures.<extension> will be created. If the directory does not exist, it will be created.
-            -If 'output_path=/path/to/directory/some_file_name.arrow' then this file will be created. If the directory does not exist, it will also be created.
-            -If 'output_path=some_file_name.arrow' then this file will be created in the current working directory.
+            The output_path can either be to a directory or filename. For example,
+                - If 'output_path=/path/to/directory' then a file with the default name NyxusFeatures.<extension> will be created. If the directory does not exist, it will be created.
+                - If 'output_path=/path/to/directory/some_file_name.arrow' then this file will be created. If the directory does not exist, it will also be created.
+                - If 'output_path=some_file_name.arrow' then this file will be created in the current working directory.
 
         Returns
         -------
@@ -326,10 +326,10 @@ class Nyxus:
             Output format for the features values. Valid options are "pandas", "arrowipc", and "parquet".
         output_path: str (optional, default "")
             Output filepath for Arrow IPC and Parquet output formats. Default is "", which is the current directory.
-            The output_path can either be to a directory or filename. For example:
-            -If 'output_path=/path/to/directory' then a file with the default name NyxusFeatures.<extension> will be created. If the directory does not exist, it will be created.
-            -If 'output_path=/path/to/directory/some_file_name.arrow' then this file will be created. If the directory does not exist, it will also be created.
-            -If 'output_path=some_file_name.arrow' then this file will be created in the current working directory.
+            The output_path can either be to a directory or filename. For example,
+                - If 'output_path=/path/to/directory' then a file with the default name NyxusFeatures.<extension> will be created. If the directory does not exist, it will be created.
+                - If 'output_path=/path/to/directory/some_file_name.arrow' then this file will be created. If the directory does not exist, it will also be created.
+                - If 'output_path=some_file_name.arrow' then this file will be created in the current working directory.
             
         Returns
         -------
@@ -452,10 +452,10 @@ class Nyxus:
             Output format for the features values. Valid options are "pandas", "arrowipc", and "parquet".
         output_path: str (optional, default "")
             Output filepath for Arrow IPC and Parquet output formats. Default is "", which is the current directory.
-            The output_path can either be to a directory or filename. For example:
-            -If 'output_path=/path/to/directory' then a file with the default name NyxusFeatures.<extension> will be created. If the directory does not exist, it will be created.
-            -If 'output_path=/path/to/directory/some_file_name.arrow' then this file will be created. If the directory does not exist, it will also be created.
-            -If 'output_path=some_file_name.arrow' then this file will be created in the current working directory.
+            The output_path can either be to a directory or filename. For example,
+                - If 'output_path=/path/to/directory' then a file with the default name NyxusFeatures.<extension> will be created. If the directory does not exist, it will be created.
+                - If 'output_path=/path/to/directory/some_file_name.arrow' then this file will be created. If the directory does not exist, it will also be created.
+                - If 'output_path=some_file_name.arrow' then this file will be created in the current working directory.
 
         Returns
         -------
