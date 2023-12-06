eb74d4d46096d8dcd4d5e353d3de70d6663da8c3
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2023-12-06 14:32:48 -0700

    Change permissions for the temp directory

--- tests/test_arrow.h
+++ tests/test_arrow.h
@@ -221,7 +221,13 @@ bool are_tables_equal(const arrow::Table& table1, const arrow::Table& table2) {
 
 void test_arrow() {
 
-    auto temp = fs::temp_directory_path();
+    auto temp = fs::temp_directory_path()/"nyxus_temp/";
+
+    if(!fs::exists(temp)) {
+        auto created = fs::create_directory(temp);
+    }
+
+    fs::permissions(temp, fs::perms::all);
 
     std::string outputPath = temp.u8string() + "NyxusFeatures.arrow";
 
@@ -272,7 +278,7 @@ void test_arrow() {
     ASSERT_TRUE(are_tables_equal(*results_table, *features_table));
 
 
-    auto is_deleted = fs::remove(outputPath);
+    auto is_deleted = fs::remove_all(temp);
 
     if(!is_deleted) {
         FAIL() << "Error deleting arrow file." << std::endl;
@@ -281,8 +287,14 @@ void test_arrow() {
 
 void test_parquet() {
 
-    auto temp = fs::temp_directory_path();
+    auto temp = fs::temp_directory_path()/"nyxus_temp/";
 
+    if(!fs::exists(temp)) {
+        auto created = fs::create_directory(temp);
+    }
+
+    fs::permissions(temp, fs::perms::all);
+    
     std::string outputPath = temp.u8string() + "NyxusFeatures.parquet";
 
     auto arrow_stream = ArrowOutputStream();
@@ -332,7 +344,7 @@ void test_parquet() {
     ASSERT_TRUE(are_tables_equal(*results_table, *features_table));
 
 
-    auto is_deleted = fs::remove(outputPath);
+    auto is_deleted = fs::remove_all(temp);
 
     if(!is_deleted) {
         FAIL() << "Error deleting arrow file." << std::endl;
