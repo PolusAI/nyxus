97c9604240f641d396d2306fc6cec9880eaf18ac
Sameeul B Samee <sameeul@gmail.com>
2023-12-11 18:47:29 -0500

    update column

--- src/nyx/output_2_buffer.cpp
+++ src/nyx/output_2_buffer.cpp
@@ -71,7 +71,8 @@ namespace Nyxus
 
 				// --GLRLM family
 				bool glrlmFeature = std::find (GLRLMFeature::featureset.begin(), GLRLMFeature::featureset.end(), fc) != GLRLMFeature::featureset.end();
-				if (glrlmFeature)
+				bool nonAngledGlrlmFeature = std::find(GLRLMFeature::nonAngledFeatures.begin(), GLRLMFeature::nonAngledFeatures.end(), fc) != GLRLMFeature::nonAngledFeatures.end(); // prevent output of a non-angled feature in an angled way
+				if (glrlmFeature && nonAngledGlrlmFeature == false)
 				{
 					// Populate with angles
 					for (auto ang : GLRLMFeature::rotAngles)
