c205bb33671886ab96533cfd65175350f791e517
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2024-01-04 12:34:57 -0700

    Fix path

--- tests/python/test_tissuenet_data.py
+++ tests/python/test_tissuenet_data.py
@@ -4,16 +4,16 @@ from pathlib import Path
 
 path = str(Path(__file__).parent.resolve())
 
-with BioReader('/data/int/p0_y1_r1_c0.ome.tif') as br:
+with BioReader(path + '/data/int/p0_y1_r1_c0.ome.tif') as br:
     int_p0_y1_r1_c0 = br.read()
     
-with BioReader('/data/int/p0_y1_r1_c1.ome.tif') as br:
+with BioReader(path + '/data/int/p0_y1_r1_c1.ome.tif') as br:
     int_p0_y1_r1_c1 = br.read()
     
-with BioReader('/data/seg/p0_y1_r1_c0.ome.tif') as br:
+with BioReader(path + '/data/seg/p0_y1_r1_c0.ome.tif') as br:
     seg_p0_y1_r1_c0 = br.read()
     
-with BioReader('/data/seg/p0_y1_r1_c1.ome.tif') as br:
+with BioReader(path + '/data/seg/p0_y1_r1_c1.ome.tif') as br:
     seg_p0_y1_r1_c1 = br.read()
 
 tissuenet_int = np.array([int_p0_y1_r1_c0, int_p0_y1_r1_c1])
