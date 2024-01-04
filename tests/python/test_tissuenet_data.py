b68c58a188c04b6e4872f2f9534adfda22f10cb9
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2024-01-04 12:05:02 -0700

    Remove old method for reading images

--- tests/python/test_tissuenet_data.py
+++ tests/python/test_tissuenet_data.py
@@ -1,23 +1,5 @@
-from PIL import Image
 import numpy as np
 from bfio import BioReader
-
-'''
-int_p0_y1_r1_c0 = Image.open('/Users/jmckinzie/Documents/GitHub/nyxus/tests/python/data/int/p0_y1_r1_c0.ome.tif')
-int_p0_y1_r1_c1 = Image.open('/Users/jmckinzie/Documents/GitHub/nyxus/tests/python/data/int/p0_y1_r1_c1.ome.tif')
-
-seg_p0_y1_r1_c0 = Image.open('/Users/jmckinzie/Documents/GitHub/nyxus/tests/python/data/seg/p0_y1_r1_c0.ome.tif')
-seg_p0_y1_r1_c1 = Image.open('/Users/jmckinzie/Documents/GitHub/nyxus/tests/python/data/seg/p0_y1_r1_c1.ome.tif')
-
-int_p0_y1_r1_c0 = np.array(int_p0_y1_r1_c0)
-int_p0_y1_r1_c1 = np.array(int_p0_y1_r1_c1)
-
-seg_p0_y1_r1_c0 = np.array(seg_p0_y1_r1_c0)
-seg_p0_y1_r1_c1 = np.array(seg_p0_y1_r1_c1)
-
-tissuenet_int = np.array([int_p0_y1_r1_c0, int_p0_y1_r1_c1])
-tissuenet_seg = np.array([seg_p0_y1_r1_c0, seg_p0_y1_r1_c1])
-'''
 
 with BioReader('/Users/jmckinzie/Documents/GitHub/nyxus/tests/python/data/int/p0_y1_r1_c0.ome.tif') as br:
     int_p0_y1_r1_c0 = br.read()
