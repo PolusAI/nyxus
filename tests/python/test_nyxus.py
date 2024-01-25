3448f1a3efec9ff983b48e36ef6a47c7559ada2f
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2024-01-25 07:47:11 -0700

    Mark gpu tests

--- tests/python/test_nyxus.py
+++ tests/python/test_nyxus.py
@@ -85,7 +85,8 @@ class TestNyxus():
                         break
             
             assert len(montage_not_equal) == 0
-            
+        
+        @pytest.mark.gpu
         def test_gabor_gpu(self):
             # cpu gabor
             cpu_nyx = nyxus.Nyxus(["GABOR"])
