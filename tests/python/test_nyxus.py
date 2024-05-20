9fc3d1a16f47c49e485d1eebb9b425ed417de4eb
JesseMckinzie <jessemckinzie145@gmail.com>
2024-05-20 12:25:24 -0600

    Update ImageQuality tests

--- tests/python/test_nyxus.py
+++ tests/python/test_nyxus.py
@@ -650,11 +650,56 @@ class TestNyxus():
             data_path = path + '/data/'
             
             nyx = nyxus.ImageQuality (["*ALL_IMQ*"])
-            assert nyx is not None
-            
-            montage_features = nyx.featurize(tissuenet_int, tissuenet_seg, intensity_names=['p0_y1_r1_c0.ome.tif', 'p0_y1_r1_c1.ome.tif'], label_names=['p0_y1_r1_c0.ome.tif', 'p0_y1_r1_c1.ome.tif'])
+
             directory_features = nyx.featurize_directory(data_path + 'int/', data_path + 'seg/')      
 
-            assert montage_features.shape[1] > 3
+            assert directory_features.shape[1] > 3
+
+        def test_image_quality_montage(self):
+            '''
+            Test that ImageQuality class can be imported and will calculated features.
+            Numerical tests are in C++ tests
+            '''
+            
+            path = str(pathlib.Path(__file__).parent.resolve())
+            
+            data_path = path + '/data/'
+            
+            nyx = nyxus.ImageQuality (["*ALL_IMQ*"])
+
+            directory_features = nyx.featurize(tissuenet_int, tissuenet_seg, intensity_names=['p0_y1_r1_c0.ome.tif', 'p0_y1_r1_c1.ome.tif'], label_names=['p0_y1_r1_c0.ome.tif', 'p0_y1_r1_c1.ome.tif'])    
+
+            assert directory_features.shape[1] > 3
+        
+        def test_image_quality_single_roi(self):
+            '''
+            Test that ImageQuality class can be imported and will calculated features.
+            Numerical tests are in C++ tests
+            '''
+            
+            path = str(pathlib.Path(__file__).parent.resolve())
+            
+            data_path = path + '/data/'
+            
+            nyx = nyxus.ImageQuality (["*ALL_IMQ*"])
+
+            directory_features = nyx.featurize_directory(data_path + 'int/', data_path + 'int/')      
+
+            assert directory_features.shape[1] > 3
+
+        def test_image_quality_montage_single_roi(self):
+            '''
+            Test that ImageQuality class can be imported and will calculated features.
+            Numerical tests are in C++ tests
+            '''
+            
+            path = str(pathlib.Path(__file__).parent.resolve())
+            
+            data_path = path + '/data/'
+            
+            nyx = nyxus.ImageQuality (["*ALL_IMQ*"])
+
+            directory_features = nyx.featurize(tissuenet_int, intensity_names=['p0_y1_r1_c0.ome.tif', 'p0_y1_r1_c1.ome.tif'], label_names=['p0_y1_r1_c0.ome.tif', 'p0_y1_r1_c1.ome.tif'])    
+
             assert directory_features.shape[1] > 3
 
