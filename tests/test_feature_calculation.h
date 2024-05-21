ecaf7ba3708d52d4dd3f4d5be606abab6e4e3f2d
JesseMckinzie <jessemckinzie145@gmail.com>
2024-05-21 08:12:27 -0600

    Add test functions to Nyxus namespace

--- tests/test_feature_calculation.h
+++ tests/test_feature_calculation.h
@@ -7,34 +7,36 @@
 #include "../src/nyx/environment.h"
 #include "test_data.h"
 #include "test_main_nyxus.h"
+namespace Nyxus
+{
+    // check value of single feature
+    void test_truth(const std::vector<double> values, double truth_value, double frac_tolerance) {
+        ASSERT_TRUE(agrees_gt(values[0], truth_value, frac_tolerance));
+    }
 
-// check value of single feature
-void test_truth(const std::vector<double> values, double truth_value, double frac_tolerance) {
-    ASSERT_TRUE(agrees_gt(values[0], truth_value, frac_tolerance));
-}
+    template <class T, class S, class F>
+    void test_feature(const T& feature_object,
+                    const F& feature_name,
+                    const int num_feautures, 
+                    const NyxusPixel* intensity_data, 
+                    const NyxusPixel* mask_data, 
+                    int image_size,
+                    const S& truth_value,
+                    double frac_tolerance = 1000) {
+        
+        LR roidata;
+        // Calculate features
+        T f;
 
-template <class T, class S, class F>
-void test_feature(const T& feature_object,
-                  const F& feature_name,
-                  const int num_feautures, 
-                  const NyxusPixel* intensity_data, 
-                  const NyxusPixel* mask_data, 
-                  int image_size,
-                  const S& truth_value,
-                  double frac_tolerance = 1000) {
-    
-    LR roidata;
-    // Calculate features
-    T f;
+        load_masked_test_roi_data (roidata, intensity_data, mask_data, image_size);
+        ASSERT_NO_THROW(f.calculate(roidata));
 
-    load_masked_test_roi_data (roidata, intensity_data, mask_data, image_size);
-    ASSERT_NO_THROW(f.calculate(roidata));
+        // Initialize per-ROI feature value buffer with zeros
+        roidata.initialize_fvals();
 
-    // Initialize per-ROI feature value buffer with zeros
-    roidata.initialize_fvals();
+        // Retrieve values of the features implemented by class 'PixelIntensityFeatures' into ROI's feature buffer
+        f.save_value(roidata.fvals);
 
-    // Retrieve values of the features implemented by class 'PixelIntensityFeatures' into ROI's feature buffer
-    f.save_value(roidata.fvals);
-
-    test_truth(roidata.fvals[(int)feature_name], truth_value, frac_tolerance);
+        test_truth(roidata.fvals[(int)feature_name], truth_value, frac_tolerance);
+    };
 };
