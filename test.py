7134cc6123ec73a242675a15b03c9299c1e0d4e5
JesseMckinzie <jessemckinzie145@gmail.com>
2024-05-21 08:14:25 -0600

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from nyxus import Nyxus 
-
-
-nyx = Nyxus(["FOCUS_SCORE"])
-
-features = nyx.featurize_directory("/Users/mckinziejr/Documents/GitHub/nyxus/data/int", "/Users/mckinziejr/Documents/GitHub/nyxus/data/int")
-
-print(features)
