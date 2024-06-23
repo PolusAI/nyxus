86bb2d34ea902fe6ef49b51dc4ba456cc62051da
Sameeul Bashir Samee <sameeul@gmail.com>
2024-06-23 18:58:15 -0400

    update pybind11 for numpy 2.0 support (#222)
    update pybind11 for numpy 2.0 support

--- ci-utils/install_prereq_linux.sh
+++ ci-utils/install_prereq_linux.sh
@@ -47,9 +47,9 @@ fi
 mkdir -p "$LOCAL_INSTALL_DIR"
 mkdir -p "$LOCAL_INSTALL_DIR"/include
 
-curl -L https://github.com/pybind/pybind11/archive/refs/tags/v2.11.1.zip -o v2.11.1.zip
-unzip v2.11.1.zip
-cd pybind11-2.11.1
+curl -L https://github.com/pybind/pybind11/archive/refs/tags/v2.12.0.zip -o v2.12.0.zip
+unzip v2.12.0.zip
+cd pybind11-2.12.0
 mkdir build_man
 cd build_man
 cmake -DCMAKE_INSTALL_PREFIX=../../"$LOCAL_INSTALL_DIR"/  -DPYBIND11_TEST=OFF ..
