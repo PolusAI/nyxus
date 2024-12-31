10d84830292b9d26e8c96e100ba261b940cbd961
andriy <85194761+friskluft@users.noreply.github.com>
2024-12-31 18:13:59 -0500

    POL5609 3d-glrlm (#249)

---
+++ src/nyx/features/3d_glrlm_nontriv.cpp
@@ -0,0 +1,11 @@
+#include "../environment.h"
+#include "3d_glrlm.h"
+#include "image_matrix_nontriv.h"
+
+using namespace Nyxus;
+
+void D3_GLRLM_feature::osized_calculate (LR& r, ImageLoader& imloader)
+{
+	calculate(r);
+}
+
