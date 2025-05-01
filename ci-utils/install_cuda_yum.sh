f77be674cb744236233cc4d3802a6ea0908d26c1
Sameeul B Samee <sameeul@gmail.com>
2025-05-01 12:43:36 -0400

    Update wheel building process

--- ci-utils/install_cuda_yum.sh
+++ ci-utils/install_cuda_yum.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 # Usage: $bash install_cuda_yum.sh <VERSION>
 
-yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-rhel7.repo &&
-yum clean all
+dnf config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda-rhel8.repo &&
+dnf clean all &&
+dnf -y module install nvidia-driver:latest-dkms
 
 version=$1
 
 if [ $version -eq 11 ]; then
     echo "Installing cuda toolkit 11"
-    yum -y install cuda-toolkit-11-3-11.3.1-1
+    dnf -y install cuda-11-8
+    echo 'export PATH=/usr/local/cuda-11.8/bin:$PATH' >> ~/.bashrc
+    echo 'export LD_LIBRARY_PATH=/usr/local/cuda-11.8/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
+    source ~/.bashrc
 elif [ $version -eq 12 ]; then
     echo "Installing cuda toolkit 12"
-    yum -y install cuda-toolkit-12-3-12.3.1-1
+    dnf -y install cuda-12-3
+    echo 'export PATH=/usr/local/cuda-12.3/bin:$PATH' >> ~/.bashrc
+    echo 'export LD_LIBRARY_PATH=/usr/local/cuda-12.3/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
+    source ~/.bashrc
 else
     echo "Invalid version. Please provide either 11 or 12."
     exit 1
