7ae7c2b1b32b61f74d4d4f4a6c4341f0ba0ebda4
Sameeul B Samee <sameeul@gmail.com>
2025-05-02 13:54:53 -0400

    update cuda install

--- ci-utils/install_cuda_yum.sh
+++ ci-utils/install_cuda_yum.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 # Usage: $bash install_cuda_yum.sh <VERSION>
 
-dnf config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel8/x86_64/cuda-rhel8.repo &&
-dnf clean all &&
-dnf -y module install nvidia-driver:latest-dkms
+yum config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel8/x86_64/cuda-rhel8.repo &&
+yum clean all &&
+yum -y module install nvidia-driver:latest-dkms
 
 version=$1
 
 if [ $version -eq 11 ]; then
     echo "Installing cuda toolkit 11"
-    dnf -y install cuda-11-8
+    yum -y install cuda-11-8
     echo 'export PATH=/usr/local/cuda-11.8/bin:$PATH' >> ~/.bashrc
     echo 'export LD_LIBRARY_PATH=/usr/local/cuda-11.8/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
     source ~/.bashrc
 elif [ $version -eq 12 ]; then
     echo "Installing cuda toolkit 12"
-    dnf -y install cuda-12-3
+    yum -y install cuda-12-3
     echo 'export PATH=/usr/local/cuda-12.3/bin:$PATH' >> ~/.bashrc
     echo 'export LD_LIBRARY_PATH=/usr/local/cuda-12.3/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
     source ~/.bashrc
