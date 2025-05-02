8ec367d3ea2a4c2f5cd5f4a36771de476ff0b45f
Sameeul B Samee <sameeul@gmail.com>
2025-05-02 11:28:24 -0400

    Update arrow

--- ci-utils/install_prereq_linux.sh
+++ ci-utils/install_prereq_linux.sh
@@ -237,9 +237,9 @@ fi
 
 if [[ $BUILD_ARROW_DEP -eq 1 ]]; then
 
-    curl -L https://github.com/apache/arrow/archive/refs/tags/apache-arrow-19.0.0.zip -o  arrow-apache-arrow-19.0.0.zip
-    unzip arrow-apache-arrow-19.0.0.zip
-    cd arrow-apache-arrow-19.0.0
+    curl -L https://github.com/apache/arrow/archive/refs/tags/apache-arrow-20.0.0.zip -o  arrow-apache-arrow-20.0.0.zip
+    unzip arrow-apache-arrow-20.0.0.zip
+    cd arrow-apache-arrow-20.0.0
     cd cpp
     mkdir build
     cd build
