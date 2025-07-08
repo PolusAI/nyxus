134f8838570f16a2ab1d619858faccc82a3dee67
Sameeul B Samee <sameeul@gmail.com>
2025-07-08 13:01:37 -0400

    update ci workflow

--- ci-utils/install_cuda_yum.sh
+++ ci-utils/install_cuda_yum.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 # Usage: $bash install_cuda_yum.sh <VERSION>
 
-yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-rhel7.repo &&
-yum clean all &&
-yum -y module install nvidia-driver:latest-dkms
-
 version=$1
 
 if [ $version -eq 11 ]; then
+    yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-rhel7.repo
+    yum clean all
+    yum -y module install nvidia-driver:latest-dkms
     echo "Installing cuda toolkit 11"
     yum -y install cuda-11-8
     echo 'export PATH=/usr/local/cuda-11.8/bin:$PATH' >> ~/.bashrc
     echo 'export LD_LIBRARY_PATH=/usr/local/cuda-11.8/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
     source ~/.bashrc
 elif [ $version -eq 12 ]; then
+    dnf config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel8/x86_64/cuda-rhel8.repo
+    dnf clean all
+    dnf -y module install nvidia-driver:latest-dkms
+    dnf -y install cuda-toolkit-12-8
     echo "Installing cuda toolkit 12"
-    yum -y install cuda-12-3
-    echo 'export PATH=/usr/local/cuda-12.3/bin:$PATH' >> ~/.bashrc
-    echo 'export LD_LIBRARY_PATH=/usr/local/cuda-12.3/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
+    echo 'export PATH=/usr/local/cuda-12.8/bin:$PATH' >> ~/.bashrc
+    echo 'export LD_LIBRARY_PATH=/usr/local/cuda-12.8/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
     source ~/.bashrc
 else
     echo "Invalid version. Please provide either 11 or 12."
